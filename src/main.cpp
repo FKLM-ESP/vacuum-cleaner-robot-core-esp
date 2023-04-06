d77e4d0d7fc5f475a36836677367b9845ebfd88e
Filip Georgiev <filip.vic.georgiev@gmail.com>
2023-04-06 16:01:22 +0300

    Added skeleton state behaviour, which can be simulated with button press

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,60 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
 
-#define WAIT_TIME_MS 500 
 DigitalOut led1(LED1);
+DigitalIn button(USER_BUTTON);
 
+enum control_mode {automatic, manual} mode;
+
+bool buttonDown = false;
+
+void autoClean()
+{
+    //TODO: handle automatic cleaning algorithm
+}
+
+void handleControls()
+{
+    //TODO: handle control commands sent from GUI
+}
+
+void handleButton()
+{
+    //TODO: handle wifi communication here
+    if (button) {  // button is pressed
+        if  (!buttonDown) {  // a new button press
+            if (mode == automatic){
+                mode = manual;
+            }
+            else{
+                mode = automatic;
+            }
+            buttonDown = true;     // record that the button is now down so we don't count one press lots of times
+            thread_sleep_for(10);  // ignore anything for 10ms, a very basic way to de-bounce the button. 
+        } 
+    } else { // button isn't pressed
+        buttonDown = false;
+    }
+
+}
 
 int main()
 {
-    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    printf("This is the vacuum cleaner core running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+    
+    mode = manual;
 
-    while (true)
-    {
-        led1 = !led1;
-        thread_sleep_for(WAIT_TIME_MS);
+    while (true){
+        switch(mode){
+            case manual:
+                led1 = true;
+                handleButton();
+                handleControls();
+                break;
+            case automatic:
+                led1 = false;
+                handleButton();
+                autoClean();
+                break;
+        }
     }
 }
