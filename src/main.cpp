bb156399986ab01912c3767027f1f02cd8fa5c30
Lorenzo Medici <lo.medici@outlook.com>
2023-05-17 17:13:52 +0300

    Filled in readBattery function that reads and calculates the battery percentage

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,9 @@ int coords[MAX_COORDS] = {};
 
 const int bytesPerIMUValue = sizeof(float);
 
+// battery monitor
+AnalogIn battery_reader(PB_1);
+
 // general control variables
 DigitalOut led1(LED1);
 DigitalIn button(USER_BUTTON);
@@ -54,8 +57,23 @@ bool buttonDown = false;
 
 uint8_t readBattery()
 {
-    // TODO: read battery level
-    return 100;
+    float full_battery_voltage = 12.6;
+    float empty_battery_voltage = 9.6;
+
+    // ~0.76, indicates the level in [0,1] at which the battery is considered empty, to use as
+    //   reference as the 0% value
+    float scaled_empty_battery_voltage = empty_battery_voltage / full_battery_voltage;
+
+    // In range [0, 1], meaning in range [0v, 3.3v]
+    float read_scaled_battery_voltage = battery_reader.read();
+
+    // In range [0, 1], meaning in range [2.51v, 3.3v] from analog pin, meaning [9.6v, 12.6v] at battery
+    // The percentage is calculated by subtracting the scaled empty battery value from read value,
+    //   then dividing by the "usable" scaled battery voltage range (1 - scaled range)
+    float battery_percentage = (read_scaled_battery_voltage - scaled_empty_battery_voltage) / 
+                                (1 - scaled_empty_battery_voltage);
+
+    return (uint8_t) (battery_percentage * 100);
 }
 
 void sendBattery(TCPSocket *socket)
